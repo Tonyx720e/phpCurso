4399157413961a79b89d286ff138a47e3f83f8f0
Tony Medina <cayapadeveloper@gmail.com>
2024-11-21 11:23:28 +0100

    aprendiendo PHP

--- index.php
+++ index.php
@@ -1,2 +1,41 @@
 <?php
-phpinfo();
+$nombre = "Tony";
+$isDev = true;
+if(true){
+    $isDev = "Si es desarrollador";
+}else{
+    $isDev = "No es desarollado y es quedado";
+}
+$edad = "34";
+var_dump($nombre);
+var_dump($isDev);
+var_dump($edad);
+$output = "<script> alert('hola')</script> eres desarrollador? <br />  ";
+const CONEXION = "db";
+?>
+<h1>
+    <?= "hola " 
+    . $nombre 
+    . " $output"
+    . $isDev
+    . " y su edad es "
+    . $edad
+    . " Tiene una conexio de "
+    . CONEXION
+    ?>
+</h1>
+
+<ol>
+    <ul><?= $isDev ?></ul>
+    <ul><?= $edad ?></ul>
+</ol>
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+</style>
